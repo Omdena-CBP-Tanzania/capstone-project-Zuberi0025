a76ea6618488ea60671c3a121d22e5a2f7b4f3cb
Zuberi0025 <mabalozy22@gmail.com>
2025-05-12 13:22:14 +0300

    add

--- visualize.py
+++ visualize.py
@@ -31,7 +31,7 @@ def plot_seasonal_pattern(df1):
     """"Monthly Rainfall distribution"""
     fig,ax=plt.subplots(figsize=(8,5))
     avg=df1.groupby('Year')['PREC'].mean().reset_index()
-    ax.plot(avg["Year"],avg["PREC"],marker='--',color='green')
+    ax.plot(avg["Year"],avg["PREC"],marker='o',color='green')
     ax.set_xlabel("month")
     ax.set_ylabel("Rainfall")
     ax.set_title("Monthly Rainfall distribution")
