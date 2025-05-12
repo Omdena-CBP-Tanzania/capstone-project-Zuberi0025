33c6668ed477d1fc087befdb18e3f07da9074dbc
Zuberi0025 <mabalozy22@gmail.com>
2025-05-12 19:45:14 +0300

    add

--- predict.py
+++ predict.py
@@ -8,15 +8,15 @@ def make_prediction(model,year,month,day,max,min):
     return model.predict(features)[0]
 
 #Get Historical context
-def get_historical_context(df1, month):
-    years=df1['Month'].unique()
+def get_historical_context(df1, day):
+    years=df1['Year'].unique()
     hist_temps=[]
     
     for year in years:
-        month_data=df1[(df1['Month']==year)&(df1['Date']==month)]
+        month_data=df1[(df1['Year']==year)&(df1['Date']==day)]
         if not month_data.empty:
             hist_temps.append((year,month_data['Rainfall'].values[0]))
     return hist_temps
 
-def get_historical_average(df,month):
-    return df[df['Date']==month]['Rainfall'].sum()
+def get_historical_average(df,day):
+    return df[df['Date']==day]['Rainfall'].sum()
