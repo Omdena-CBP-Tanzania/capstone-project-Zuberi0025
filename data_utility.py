1df320c0a5400b31baa7a53924dfa694e3dd734a
Zuberi0025 <mabalozy22@gmail.com>
2025-05-13 08:24:03 +0300

    make changes

--- data_utility.py
+++ data_utility.py
@@ -24,24 +24,3 @@ def prepare_features(df1):
     return X, y
 
 ##Prepare features for LSTM Model
-
-##Normalize data for LSTM Model
-def normalize_data(X):
-    scaler=MinMaxScaler()
-    X_scaled=scaler.fit_transform(X)
-    n_past=30
-    return X_scaled,scaler
-def createXY(df,n_past):
-    """Create a sequence of past observations X and their corresponding next values Y"""
-    if not isinstance(df,np.ndarray):
-        raise ValueError("Input dataset should be array")
-    if len(df.shape)!=2:
-        raise ValueError("Input data must be 2D")
-    if n_past>=len(df):
-        raise ValueError("n_past must be smaller than the dataset length")
-    
-    dataX,dataY=[],[]
-    for i in range(n_past,len(df)):
-        dataX.append(df[i-n_past:i,0:df.shape[1]])
-        dataY.append(df[i,0])
-    return np.array(dataX),np.array(dataY)  
--- model_utils.py
+++ model_utils.py
@@ -6,8 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error,root_mean_squared_error,r2_score
-from tensorflow.keras.models import Sequantial
-from tensorflow.keras.layers import Dense,Droupout,Flatten,LSTM
 
 #Data split
 def split_data(X,y,test_size=0.2):
